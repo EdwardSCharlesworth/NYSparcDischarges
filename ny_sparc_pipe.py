e2f094c06840619750c8132b8524a169727013ff
EdwardSCharlesworth <edwardscharlesworth@gmail.com>
2019-03-11 14:30:52 -0400

    tweeks

--- ny_sparc_pipe.py
+++ ny_sparc_pipe.py
@@ -12,30 +12,30 @@ from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import dump_svmlight_file
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, precision_recall_curve, roc_auc_score, roc_curve, average_precision_score
+from sklearn.metrics import accuracy_score,precision_recall_curve,roc_auc_score, roc_curve, average_precision_score
 import matplotlib.pyplot as plt
 from sklearn.utils.fixes import signature
 
-file_2014='/home/ed/Downloads/ny_SPARCS_discharges_2014.csv'
+file_2014 = '/home/ed/Downloads/ny_SPARCS_discharges_2014.csv'
 #replace with scraper
-file_2015='/home/ed/Downloads/ny_SPARCS_discharges_2015.csv'
+file_2015 = '/home/ed/Downloads/ny_SPARCS_discharges_2015.csv'
 
-sparcs_2015=pd.read_csv(file_2015,engine='python')
-sparcs_2014=pd.read_csv(file_2015,engine='python')
+sparcs_2015 = pd.read_csv(file_2015,engine='python')
+sparcs_2014 = pd.read_csv(file_2015,engine='python')
 
 def clean(data):
     data.columns = data.columns.str.strip().str.upper().str\
     .replace(' ', '_').str.replace('(', '').str.replace(')', '')
     return data
 
-sparcs_2015=clean(sparcs_2015)
-sparcs_2014=clean(sparcs_2014)
+sparcs_2015 = clean(sparcs_2015)
+sparcs_2014 = clean(sparcs_2014)
 #2015
 numeric_cols = [cname for cname in sparcs_2015.columns if
                 sparcs_2015[cname].dtype in ['int64', 'float64']]
 for columns in numeric_cols:
     sparcs_2015[columns].fillna(0, inplace=True)
-    
+
 text_cols = [cname for cname in sparcs_2015.columns if
              sparcs_2015[cname].dtype not in ['int64', 'float64']]
 
@@ -43,25 +43,25 @@ for columns in text_cols:
     sparcs_2015[columns].fillna("NA", inplace=True)
 
 #2014
-numeric_cols=[cname for cname in sparcs_2014.columns if
+numeric_cols = [cname for cname in sparcs_2014.columns if
               sparcs_2014[cname].dtype in ['int64', 'float64']]
 
 for columns in numeric_cols:
     sparcs_2014[columns].fillna(0, inplace=True)
 
-text_cols=[cname for cname in sparcs_2014.columns if
+text_cols = [cname for cname in sparcs_2014.columns if
            sparcs_2014[cname].dtype not in ['int64', 'float64']]
 
 for columns in text_cols:
     sparcs_2014[columns].fillna("NA", inplace=True)
 #%%
 le = LabelEncoder()
-sparcs_2015['APR_RISK_OF_MORTALITY']=sparcs_2015['APR_RISK_OF_MORTALITY'].replace("NA","Minor")
+sparcs_2015['APR_RISK_OF_MORTALITY'] = sparcs_2015['APR_RISK_OF_MORTALITY'].replace("NA","Minor")
 le.fit(["Minor", "Moderate", "Major", "Extreme"])
-y=le.transform(sparcs_2015['APR_RISK_OF_MORTALITY'])
+y = le.transform(sparcs_2015['APR_RISK_OF_MORTALITY'])
 
 equiv_mort = {'Minor':0, 'Moderate':1, 'Major':0, 'Extreme': 0}
-sparcs_2015['y_binary']=sparcs_2015['APR_RISK_OF_MORTALITY'].map(equiv_mort)
+sparcs_2015['y_binary'] = sparcs_2015['APR_RISK_OF_MORTALITY'].map(equiv_mort)
 y_binary=sparcs_2015['y_binary'].values
 
 proposed_X = sparcs_2015.drop(['y_binary','TOTAL_CHARGES','TOTAL_COSTS','APR_RISK_OF_MORTALITY',
@@ -84,7 +84,7 @@ one_hot_encoded_X=clean(one_hot_encoded_X)
 
 features=list(one_hot_encoded_X.columns)
 #%%
-X_train, X_test, y_train, y_test =train_test_split(one_hot_encoded_X,
+X_train, X_test, y_train, y_test = train_test_split(one_hot_encoded_X,
                                                    y,test_size=0.2,
                                                    random_state=4)
 
@@ -107,13 +107,22 @@ param = {
 num_round = 50  # the number of training iterations
 watchlist = [(dtest_svm,'eval'), (dtrain_svm,'train')]
 
+#    simple rules of the road
+#    Underfitting – Validation and training error high
+#
+#    Overfitting – Validation error is high, training error low
+#
+#    Good fit – Validation error low, slightly higher than the training error
+#
+#    Unknown fit - Validation error low, training error 'high'
+
 evals_result = {}
 bst = xgb.train(param,dtrain_svm,
                 num_round,watchlist,
                 evals_result=evals_result,
                 early_stopping_rounds=2)
 
-bst.dump_model('/home/ed/Desktop/nysparcdischarges/dump.raw.txt')
+bst.dump_model('/home/ed/Desktop/dump.raw.txt')
 
 preds = bst.predict(dtest_svm)
 y_score=preds
@@ -153,7 +162,7 @@ print('Overall AUC:', roc_auc_score(y_train, clf.predict_proba(X_train)[:,1]))
 print('Predict the probabilities on test set')
 pred = clf.predict_proba(X_train, ntree_limit=cvresult.shape[0])
 
-joblib.dump(clf, '/home/ed/Desktop/clf_model.pkl', compress=True)
+joblib.dump(clf,'/home/ed/Desktop/clf_model.pkl',compress=True)
 # bst = joblib.load('bst_model.pkl') # load it later
 
 xgb.plot_importance(clf,max_num_features=10)
@@ -196,7 +205,7 @@ plt.close()
 le = LabelEncoder()
 sparcs_2014['APR_RISK_OF_MORTALITY']=sparcs_2014['APR_RISK_OF_MORTALITY'].replace("NA","Minor")
 le.fit(["Minor", "Moderate", "Major", "Extreme"])
-y=le.transform(sparcs_2014['APR_RISK_OF_MORTALITY'])
+y = le.transform(sparcs_2014['APR_RISK_OF_MORTALITY'])
 
 equiv_mort = {'Minor':0, 'Moderate':1, 'Major':0, 'Extreme': 0}
 sparcs_2014['y_binary']=sparcs_2014['APR_RISK_OF_MORTALITY'].map(equiv_mort)
@@ -208,7 +217,7 @@ print("# of columns after one-hot encoding: {0}"\
       .format(len(one_hot_encoded_X.columns)))
 one_hot_encoded_X=clean(one_hot_encoded_X)
 
-y_pred_xgb=clf.predict_proba(one_hot_encoded_X)[:, 1]
+y_pred_xgb = clf.predict_proba(one_hot_encoded_X)[:, 1]
 fpr_grd,tpr_grd,_ =roc_curve(y_binary,y_pred_xgb)
 prediction = clf.predict(one_hot_encoded_X)
 
