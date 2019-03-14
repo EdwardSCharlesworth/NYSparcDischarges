5d63926e788c4cab044135e313577729a7a302f9
EdwardSCharlesworth <edwardscharlesworth@gmail.com>
2019-03-14 19:37:00 -0400

    xgb.cv versus gridsearch

--- sparks_pc_version.py
+++ sparks_pc_version.py
@@ -17,12 +17,12 @@ import matplotlib.pyplot as plt
 from sklearn.utils.fixes import signature
 import gc
 
-file_2014 = r"C:\Users\User\Desktop\Hospital_Inpatient_Discharges__SPARCS_De-Identified___2014.csv"
+#file_2014 = r"C:\Users\User\Desktop\Hospital_Inpatient_Discharges__SPARCS_De-Identified___2014.csv"
 #replace with scraper
 file_2015 = r"C:\Users\User\Desktop\Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv"
 
 sparcs_2015 = pd.read_csv(file_2015,engine='python')
-sparcs_2014 = pd.read_csv(file_2014,engine='python')
+#sparcs_2014 = pd.read_csv(file_2014,engine='python')
 
 def clean(data):
     data.columns = data.columns.str.strip().str.upper().str\
@@ -30,7 +30,7 @@ def clean(data):
     return data
 
 sparcs_2015 = clean(sparcs_2015)
-sparcs_2014 = clean(sparcs_2014)
+#sparcs_2014 = clean(sparcs_2014)
 #2015
 numeric_cols = [cname for cname in sparcs_2015.columns if
                 sparcs_2015[cname].dtype in ['int64', 'float64']]
@@ -43,19 +43,20 @@ text_cols = [cname for cname in sparcs_2015.columns if
 for columns in text_cols:
     sparcs_2015[columns].fillna("NA", inplace=True)
 
-#2014
-numeric_cols = [cname for cname in sparcs_2014.columns if
-              sparcs_2014[cname].dtype in ['int64', 'float64']]
-
-for columns in numeric_cols:
-    sparcs_2014[columns].fillna(0, inplace=True)
-
-text_cols = [cname for cname in sparcs_2014.columns if
-           sparcs_2014[cname].dtype not in ['int64', 'float64']]
-
-for columns in text_cols:
-    sparcs_2014[columns].fillna("NA", inplace=True)
+##2014
+#numeric_cols = [cname for cname in sparcs_2014.columns if
+#              sparcs_2014[cname].dtype in ['int64', 'float64']]
+#
+#for columns in numeric_cols:
+#    sparcs_2014[columns].fillna(0, inplace=True)
+#
+#text_cols = [cname for cname in sparcs_2014.columns if
+#           sparcs_2014[cname].dtype not in ['int64', 'float64']]
+#
+#for columns in text_cols:
+#    sparcs_2014[columns].fillna("NA", inplace=True)
 #%%
+#sparcs_2015 = sparcs_2015.sample(frac=0.01, replace=True, random_state=1).reset_index()
 le = LabelEncoder()
 sparcs_2015['APR_RISK_OF_MORTALITY'] = sparcs_2015['APR_RISK_OF_MORTALITY'].replace("NA","Minor")
 le.fit(["Minor", "Moderate", "Major", "Extreme"])
@@ -84,12 +85,53 @@ print("# of columns: {0}"\
 one_hot_encoded_X=clean(one_hot_encoded_X)
 
 features=list(one_hot_encoded_X.columns)
+
 del proposed_X
 del sparcs_2015
 gc.collect()
 #%%
+
+#using randomized search
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import ShuffleSplit
+clf_xgb = xgb.XGBClassifier(objective = 'binary:logistic')
+param_dist = {'silent': [False],
+        'max_depth': [6, 10, 15, 20],
+        'learning_rate': [0.001, 0.01, 0.1, 0.2, .3, .4, 0,3],
+        'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'colsample_bytree': [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'colsample_bylevel': [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'min_child_weight': [0.5, 1.0, 3.0, 5.0, 7.0, 10.0],
+        'gamma': [0, 0.25, 0.5, 1.0],
+        'reg_lambda': [0.1, 1.0, 5.0, 10.0, 50.0, 100.0],
+        'reg_alpha': 2. ** np.arange(-13, 10, 2),
+        'n_estimators': [100]
+             }
+clf = RandomizedSearchCV(clf_xgb, param_distributions = param_dist, 
+                         n_iter = 25, 
+                         scoring = 'f1', error_score = 0,
+                         verbose = 3, n_jobs = -1)
+
+rs = ShuffleSplit(n_splits=3, test_size=.25, random_state=0)
+gc.collect()
+
+estimators = []
+results = np.zeros(len(one_hot_encoded_X))
+score = []
+for train_index, test_index in rs.split(one_hot_encoded_X):
+#    print('Iteration:', i)
+    X_train, X_test = one_hot_encoded_X.iloc[train_index], one_hot_encoded_X.iloc[test_index]
+    y_train, y_test = new_y_binary.iloc[train_index], new_y_binary.iloc[test_index]
+    clf.fit(X_train, y_train)
+
+    estimators.append(clf.best_estimator_)
+    results[test_index] = clf.predict(X_test)
+    score.append(f1_score(y_test, results[test_index]))
+
+
+#%%
 X_train, X_test, y_train, y_test = train_test_split(one_hot_encoded_X,
-                                                   y,test_size=0.2,
+                                                   y_binary,test_size=0.2,
                                                    random_state=4)
 
 dump_svmlight_file(X_train, y_train, 'dtrain.svm', zero_based=True)
@@ -142,6 +184,7 @@ xgb.to_graphviz(bst,size="500,500!")
 
 accuracy_score(y_test, y_score)
 #%%
+
 X_train,X_test,y_train,y_test=train_test_split(one_hot_encoded_X,y_binary,test_size=0.2,random_state=4)
 xgtrain = xgb.DMatrix(X_train,label=y_train)
 clf = xgb.XGBClassifier(objective='binary:logistic',missing=np.nan,max_depth = 20,
@@ -165,9 +208,6 @@ clf.fit(X_train, y_train, eval_metric='auc')
 print('Overall AUC:', roc_auc_score(y_train, clf.predict_proba(X_train)[:,1]))
 print('Predict the probabilities on test set')
 pred = clf.predict_proba(X_train, ntree_limit=cvresult.shape[0])
-
-joblib.dump(clf,'/home/ed/Desktop/clf_model.pkl',compress=True)
-# bst = joblib.load('bst_model.pkl') # load it later
 
 xgb.plot_importance(clf,max_num_features=10)
 
@@ -206,84 +246,42 @@ plt.title('Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
 plt.show()
 plt.close()
 #%%
-#le = LabelEncoder()
-#sparcs_2014['APR_RISK_OF_MORTALITY']=sparcs_2014['APR_RISK_OF_MORTALITY'].replace("NA","Minor")
-#le.fit(["Minor", "Moderate", "Major", "Extreme"])
-#y = le.transform(sparcs_2014['APR_RISK_OF_MORTALITY'])
-#
-#equiv_mort = {'Minor':0, 'Moderate':1, 'Major':0, 'Extreme': 0}
-#sparcs_2014['y_binary']=sparcs_2014['APR_RISK_OF_MORTALITY'].map(equiv_mort)
-#y_binary=sparcs_2014['y_binary'].values
-#
-## One hot encoded
-#one_hot_encoded_X = pd.get_dummies(sparcs_2014[my_cols])
-#print("# of columns after one-hot encoding: {0}"\
-#      .format(len(one_hot_encoded_X.columns)))
-#one_hot_encoded_X=clean(one_hot_encoded_X)
-#
-#y_pred_xgb = clf.predict_proba(one_hot_encoded_X)[:, 1]
-#fpr_grd,tpr_grd,_ =roc_curve(y_binary,y_pred_xgb)
-#prediction = clf.predict(one_hot_encoded_X)
-#
-#plt.figure(1)
-#plt.plot([0, 1], [0, 1], 'k--')
-#plt.plot(fpr_grd, tpr_grd, label='xgboost cv')
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='best')
-#plt.show()
-#plt.close()
-#
-#average_precision = average_precision_score(y_binary,y_pred_xgb)
-#
-#print('Average precision-recall score: {0:0.2f}'.format(average_precision))
-#precision,recall, _ = precision_recall_curve(y_binary,y_pred_xgb)
-#step_kwargs = ({'step': 'post'}
-#               if 'step' in signature(plt.fill_between).parameters
-#               else {})
-#plt.step(recall, precision, color='b', alpha=0.2,
-#         where='post')
-#plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
-#
-#plt.xlabel('Recall')
-#plt.ylabel('Precision')
-#plt.ylim([0.0, 1.05])
-#plt.xlim([0.0, 1.0])
-#plt.title('Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
-#plt.close()
-#%%
 
-#using randomized search
-from scipy import stats
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import ShuffleSplit
-clf_xgb = xgb.XGBClassifier(objective = 'binary:logistic')
-param_dist = {'n_estimators': stats.randint(150, 500),
-              'learning_rate': stats.uniform(0.01, 0.07),
-              'subsample': stats.uniform(0.3, 0.7),
-              'max_depth': [3, 4, 5, 6, 7, 8, 9],
-              'colsample_bytree': stats.uniform(0.5, 0.45),
-              'min_child_weight': [1, 2, 3]
-             }
-clf = RandomizedSearchCV(clf_xgb, param_distributions = param_dist, n_iter = 25, scoring = 'f1', error_score = 0, verbose = 3, n_jobs = -1)
 
-rs = ShuffleSplit(n_splits=5, test_size=.25, random_state=0)
-#new_y_binary = sparcs_2015[['y_binary']] 
-X=one_hot_encoded_X
-del one_hot_encoded_X
-gc.collect()
+clf = joblib.load(r'C:\Users\User\Desktop\long_clf_model.pkl') 
+xgb.plot_importance(clf,max_num_features=10)
 
-estimators = []
-results = np.zeros(len(X))
-score = 0.0
-for train_index, test_index in rs.split(X):
-#    print('Iteration:', i)
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = new_y_binary.iloc[train_index], new_y_binary.iloc[test_index]
-    clf.fit(X_train, y_train)
+xgb.to_graphviz(clf,size="20,20!")
 
-    estimators.append(clf.best_estimator_)
-    results[test_index] = clf.predict(X_test)
-    score += f1_score(y_test, results[test_index])
-#score /= numFolds
+
+y_pred_xgb = clf.predict_proba(X_test)[:, 1]
+fpr_grd, tpr_grd, _ = roc_curve(y_test, y_pred_xgb)
+
+
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr_grd, tpr_grd, label='GBT')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend(loc='best')
+plt.show()
+plt.close()
+average_precision = average_precision_score(y_test, y_pred_xgb)
+
+print('Average precision-recall score: {0:0.2f}'.format(average_precision))
+precision,recall,_ =precision_recall_curve(y_test, y_pred_xgb)
+step_kwargs = ({'step': 'post'}
+               if 'step' in signature(plt.fill_between).parameters
+               else {})
+plt.step(recall, precision, color='b', alpha=0.2,
+         where='post')
+plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.ylim([0.0, 1.05])
+plt.xlim([0.0, 1.0])
+plt.title('Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+plt.show()
+plt.close()
